6968386fc2d6488f5aa4fc9c25136f4cac08baff
Dion Arya Pamungkas <dionarya.p@gmail.com>
2017-08-08 15:53:26 +0700

    add some features

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# Kadal.Js
+# Kadal.JS
+Suka dengan kadal ? atau kamu sering makan kadal ?, Kabar gembira bagi kamu yang suka dengan hal yang berbau kadal sekarang kadal ada Plugin Jquery nya yaitu KADAL.JS
+<br>
+### Apa Itu Kadal.js
+Kadal.js merupakan sebuah plugin jquery yang bisa membuat website kamu menampilkan gambar - gambar kadal yang keren dan mebuat website kamu lebih keren dengan adanya gambar kadal.
+<br>
+<br>
+Plugin ini di buat oleh Dion Arya Pamungkas, Alasan saya membuat plugin yang absurd ini karena saya bosan dan ingin mencoba hal yang baru wkwkkwkwwkwkk.
+
+## Instalasi
+Untuk bisa memakai plugin ini anda membutuhkan jquery terlebih dahulu bebas versi berapa aja, dan anda bisa meng-install kadal.js lewat NPM jika komputer anda sudah terpasang node.js dengan cara ketik di command line :<br>
+
+````
+npm install kadal.js
+````
+
+Atau anda juga bisa menggunakan seperti ini menggunakan tag script biasa, contoh:
+
+`````
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+<script src="kadal.js"></script>
+````
+Anda Bisa mendapatkan Kadal.JS di cdn berikut:<br>
+<code>https://dionarya6661.github.io/kadal.js</code>
+
+### Cara Menggunakan
+Siapkan terlebih dahulu file-file yang sudah disebutkan diatas yaitu jquery.min.js dan kadal.js :<br>
+````
+<div id="idnya"></div>
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+<script src="kadal.js"></script>
+
+<script>
+$(document).ready(function(){
+  $('#idnya').kadal();
+});
+
+</script>
+```
+maka akan akan telihat seperti Ini:
+
+<img src="https://image.ibb.co/eJ4rMa/capture_20170808_153835.png" width="320" height="200">
+
+Anda juga bisa menampilkan lebih dari satu gambar kadal misal anda ingin menampilkan 10 Gambar kadal,
+* Perlu diketahui hanya ada 21 gambar kadal yang hanya bisa ditampilkan
+
+Contoh menampilkan 10 gambar kadal:
+```
+<div id="idnya"></div>
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+<script src="kadal.js"></script>
+
+//anda hanya perlu menambahkan {JumlahKadal: 10}
+<script>
+$(document).ready(function(){
+  $('#idnya').kadal({
+    JumlahKadal: 10
+    });
+});
+
+</script>
+```
+
+Dan untuk mempercantik gambar kadal anda bisa menambahkan framework css bootstrap pada web anda, seperti ini : <br>
+```
+  $('#idnya').kadal({
+    JumlahKadal: 10,
+    Bootstrap: true
+    });
+
+    //namu jika sebelumnya anda sudah menambahkan bootstrap anda boleh tidak memakai nya
+
+    $('#idnya').kadal({
+      JumlahKadal: 10,
+      Bootstrap: false
+      });
+
+```
--- kadal.js
+++ kadal.js
@@ -1,46 +1,6 @@
-+function($){
-  var params = {};
-  var i;
-  var gambarKadal = new Array(
-    "https://image.ibb.co/gKSpra/the_lizard_847001_960_720.jpg",
-    "https://image.ibb.co/fdN0xF/maxresdefault.jpg",
-    "https://image.ibb.co/k70aWa/mauboya_multifasciata_kadal_reptilia.jpg",
-    "https://image.ibb.co/enNpra/lizard_316238_960_720.jpg",
-    "https://image.ibb.co/maXYHF/lizard_288221_960_720.jpg",
-    "https://image.ibb.co/cUfaWa/lizard.jpg",
-    "https://image.ibb.co/iOqaWa/Kadal_Spiderman.jpg",
-    "https://image.ibb.co/fjTjPv/kadal_obama_121230134443_577.jpg",
-    "https://image.ibb.co/fiqW4v/kadal_berpose_seperti_model.jpg",
-    "https://image.ibb.co/efLW4v/kadal.jpg",
-    "https://image.ibb.co/grkaWa/kadal_dalkijo.jpg",
-    "https://image.ibb.co/cpEr4v/k.jpg",
-    "https://image.ibb.co/cS2NBa/iguana_84633_960_720.jpg",
-    "https://image.ibb.co/iicYHF/frilled_lizard.jpg",
-    "https://image.ibb.co/mYL9ra/251015124446_kadal_ini_unik_bisa_berganti_kelamin_saat_musim.jpg",
-    "https://image.ibb.co/cXRScF/070765300_1461697211_kadal3.jpg",
-    "https://image.ibb.co/c1jfxF/20160628_buaya_20160628_161126.jpg",
-    "https://image.ibb.co/enCYHF/018775900_1461697000_kadal5.jpg",
-    "https://image.ibb.co/dOHNBa/1200px_Varanus_komodoensis1.jpg",
-    "https://image.ibb.co/fYv9ra/173614_620.jpg",
-    "https://image.ibb.co/kNeDHF/Maby_multif_F_050222_061_kng.jpg"
-  );
-          $.fn.kadal = function(params){
-
-        if (params == undefined || params.JumlahKadal == 1 || params.JumlahKadal == undefined){
-
-            this.append('<div class="col-md-4"><div class="thumbnail"><img src="'+gambarKadal[Math.floor(Math.random() * 20) + 1]+'" alt="Kadal" class="img-responsive img-thumbnail" height="210" width="350"></div></div>');
-
-        }else if(params.JumlahKadal > 1){
-
-            for(i = 0; i < parseInt(params.JumlahKadal); i++){
-              console.log(params.JumlahKadal);
-            this.append('<div class="col-md-4"><div class="thumbnail"><img src="'+gambarKadal[i]+'" alt="Kadal '+i+'" class="img-responsive img-thumbnail" height="210" width="350"></div></div>');
-          }
-
-      }else if (params.JumlahKadal > gambarKadal.length ){
-        this.append("Maaf Tidak Dapat menampilkan Gambar Kadal Sebanyak "+ params.JumlahKadal);
-      }
-
-          };
-
-}(jQuery);
+/*
+Pembuat: Dion Arya Pamungkas
+Email: dionarya.p@gmail.com
+website: https://dionarya6661.github.io
+*/
++function(a){var i,t=new Array("https://image.ibb.co/gKSpra/the_lizard_847001_960_720.jpg","https://image.ibb.co/fdN0xF/maxresdefault.jpg","https://image.ibb.co/k70aWa/mauboya_multifasciata_kadal_reptilia.jpg","https://image.ibb.co/enNpra/lizard_316238_960_720.jpg","https://image.ibb.co/maXYHF/lizard_288221_960_720.jpg","https://image.ibb.co/cUfaWa/lizard.jpg","https://image.ibb.co/iOqaWa/Kadal_Spiderman.jpg","https://image.ibb.co/fjTjPv/kadal_obama_121230134443_577.jpg","https://image.ibb.co/fiqW4v/kadal_berpose_seperti_model.jpg","https://image.ibb.co/efLW4v/kadal.jpg","https://image.ibb.co/grkaWa/kadal_dalkijo.jpg","https://image.ibb.co/cpEr4v/k.jpg","https://image.ibb.co/cS2NBa/iguana_84633_960_720.jpg","https://image.ibb.co/iicYHF/frilled_lizard.jpg","https://image.ibb.co/mYL9ra/251015124446_kadal_ini_unik_bisa_berganti_kelamin_saat_musim.jpg","https://image.ibb.co/cXRScF/070765300_1461697211_kadal3.jpg","https://image.ibb.co/c1jfxF/20160628_buaya_20160628_161126.jpg","https://image.ibb.co/enCYHF/018775900_1461697000_kadal5.jpg","https://image.ibb.co/dOHNBa/1200px_Varanus_komodoensis1.jpg","https://image.ibb.co/fYv9ra/173614_620.jpg","https://image.ibb.co/kNeDHF/Maby_multif_F_050222_061_kng.jpg");a.fn.kadal=function(a){if(void 0==a||1==a.JumlahKadal||void 0==a.JumlahKadal)this.append('<div class="col-md-4"><div class="thumbnail"><img src="'+t[Math.floor(20*Math.random())+1]+'" alt="Kadal" class="img-responsive img-thumbnail" height="210" width="350"></div></div>');else if(a.JumlahKadal>1)for(i=0;i<parseInt(a.JumlahKadal);i++)console.log(a.JumlahKadal),this.append('<div class="col-md-4"><div class="thumbnail"><img src="'+t[i]+'" alt="Kadal '+i+'" class="img-responsive img-thumbnail" height="210" width="350"></div></div>');else a.JumlahKadal>t.length&&this.append("Maaf Tidak Dapat menampilkan Gambar Kadal Sebanyak "+a.JumlahKadal)}}(jQuery);
